b88f6355ea24caa71bb5e64f6dd97d8e199c9ee5
JayvonK <jaykhuth@gmail.com>
2023-12-11 22:22:32 -0800

    student list

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,5 @@ let injectDiv = document.getElementById("injectDiv");
 let studentName = document.getElementById("studentName");
 let studentEmail = doocument.getElementById("studentEmail");
 let studentAge = document.getElementById("studentAge");
+let studentNumber = document.getElementById("studentNumber");
+
