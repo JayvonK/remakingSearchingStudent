92928698d351db62e68b29394bf4961113274c0d
JayvonK <jaykhuth@gmail.com>
2023-12-11 22:00:13 -0800

    document by id

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+let userInput = document.getElementById("userInput");
+let searchBtn = document.getElementById("searchBtn");
+let injectDiv = document.getElementById("injectDiv");
+let studentName = document.getElementById("studentName");
+let studentEmail = doocument.getElementById("studentEmail");
+let studentAge = document.getElementById("studentAge");
