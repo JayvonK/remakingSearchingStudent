bd90430046aaf304176d7badf1e1ee9ddf2eccb4
JayvonK <jaykhuth@gmail.com>
2023-12-11 23:51:47 -0800

    functions

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,25 @@ let studentEmail = doocument.getElementById("studentEmail");
 let studentAge = document.getElementById("studentAge");
 let studentNumber = document.getElementById("studentNumber");
 
+let studentList;
+
+async function ApiCall (){
+    const promise = fetch('../assets/students.json');
+    const data = promise.json();
+
+    studentList = data;
+}
+
+function findStudent (){
+    let input = userInput.value.toLowerCase();
+
+    for(let i = 0; i < studentList.length; i++){
+        if(input == studentList.firstName.toLowerCase()){
+            
+        }
+    }
+}
+
+searchBtn.addEventListener('click', function(e){
+    findStudent();
+})
