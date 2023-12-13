3977dfb90133452cd379df79d2ff3c22cafe642a
JayvonK <jaykhuth@gmail.com>
2023-12-12 23:57:17 -0800

    test

--- scripts/app.js
+++ scripts/app.js
@@ -2,29 +2,37 @@ let userInput = document.getElementById("userInput");
 let searchBtn = document.getElementById("searchBtn");
 let injectDiv = document.getElementById("injectDiv");
 let studentName = document.getElementById("studentName");
-let studentEmail = doocument.getElementById("studentEmail");
+let studentEmail = document.getElementById("studentEmail");
 let studentAge = document.getElementById("studentAge");
 let studentNumber = document.getElementById("studentNumber");
 
 let studentList;
 
 async function ApiCall (){
-    const promise = fetch('../assets/students.json');
-    const data = promise.json();
+    const promise = await fetch('../assets/students.json');
+    const data = await promise.json();
 
     studentList = data;
 }
 
 function findStudent (){
     let input = userInput.value.toLowerCase();
+    console.log("ear");
 
     for(let i = 0; i < studentList.length; i++){
-        if(input == studentList.firstName.toLowerCase()){
-            
+        let student = studentList[i];
+        if(input === student.firstName.toLowerCase()){
+            studentName.textContent = student.firstName;
+        }else{
+            injectDiv.innerHTML = "";
         }
+
     }
 }
 
 searchBtn.addEventListener('click', function(e){
     findStudent();
+    console.log("he");
 })
+
+ApiCall();
