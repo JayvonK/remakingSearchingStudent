284e2237d10f02fb848777e1ffc9b7c230a2237c
JayvonK <jaykhuth@gmail.com>
2023-12-13 22:47:56 -0800

    finish

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,7 @@ let studentAge = document.getElementById("studentAge");
 let studentNumber = document.getElementById("studentNumber");
 
 let studentList;
+let matchingStudent;
 
 async function ApiCall (){
     const promise = await fetch('../assets/students.json');
@@ -17,24 +18,33 @@ async function ApiCall (){
 
 function findStudent (){
     let input = userInput.value.toLowerCase();
-    console.log("ear");
-
-    console.log(studentList[0].firstName.toLowerCase())
+    console.log("I work");
 
     for(let i = 0; i < studentList.length; i++){
         let student = studentList[i];
         if(input === student.firstName.toLowerCase()){
-            studentName.textContent = student.firstName;
-        }else{
-            console.log("didn't work");
+            matchingStudent = student;
         }
-
     }
+
+    if(matchingStudent){
+        studentName.textContent = "Name: " + matchingStudent.firstName + " " + matchingStudent.lastName;
+        studentEmail.textContent = "Email: " + matchingStudent.email;
+        studentAge.textContent = "Age: " + matchingStudent.age;
+        studentNumber.textContent = "Number: " + matchingStudent.number;
+    } else {
+        studentName.textContent = "NO STUDENT FOUND";
+        studentEmail.textContent = "";
+        studentAge.textContent = "";
+        studentNumber.textContent = "";
+    }
+
+    userInput.value = "";
 }
 
 searchBtn.addEventListener('click', function(e){
     findStudent();
-    console.log("he");
+    console.log("CLICK WORKING");
 })
 
 ApiCall();
